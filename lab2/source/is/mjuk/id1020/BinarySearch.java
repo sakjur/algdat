e90478c19013b7012c8ee0a756c7e796cb2065a1
Emil Tullstedt <sakjur@gmail.com>
2014-11-18 23:45:05 +0100

    Fixed Binary Search

--- lab2/source/is/mjuk/id1020/BinarySearch.java
+++ lab2/source/is/mjuk/id1020/BinarySearch.java
@@ -25,37 +25,33 @@ public class BinarySearch {
     }
     public static int search(String key, String[] a, int lo, int hi) {
         // possible key indices in [lo, hi)
-		int iter = 0;
-
 		while (true) {
 			if (hi <= lo) return -1;
 			int mid = lo + (hi - lo) / 2;
 			int cmp = a[mid].compareTo(key);
 			if      (cmp > 0) hi = mid;
 			else if (cmp < 0) lo = mid+1;
-			else              return iter;
-			iter++;
+			else              return mid;
 		}
     }
 
 
     // whitelist, exception filter
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        String s = in.readAll();
-        String[] words = s.split("\\s+");
-        System.err.println("Done reading words");
+	public static void main(String [] args) {
+		In in = new In(args[0]);
+		String s = in.readAll();
+		String[] words = s.split("\\s+");
+		System.err.println("Done reading words");
 
-        // sort the words (if needed)
-        Arrays.sort(words);
-        System.err.println("Done sorting words");
+		// sort the words (if needed)
+		Arrays.sort(words);
+		System.err.println("Done sorting words");
 
-        // prompt user to enter a word and check if it's there
-        while (!StdIn.isEmpty()) {
-            String key = StdIn.readString();
-			int rv = search(key, words);
-            StdOut.println(rv);
-        }
-    }
+		// prompt user to enter a word and check if it's there
+		while (!StdIn.isEmpty()) {
+			String key = StdIn.readString();
+			if (search(key, words) < 0) StdOut.println(key);
+		}
+	}
 }
 
